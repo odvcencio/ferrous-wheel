88100dac432d599f3ce945fd60c1f2bfc29e4efa
Oscar Villavicencio <odvcencio@gmail.com>
2026-03-21 11:00:32 -0700

    add: source map //line directives
    - Add TranspileOptions struct with SourceFile field for configuring transpilation
    - Emit Go //line directives before function declarations, enums, and impl blocks
    - Wire up CLI to pass source filename, enabling debug stack traces to point to .fw files
    - Add tests for line directive generation with and without source file option

---
+++ sourcemap_test.go
@@ -0,0 +1,28 @@
+package ferrouswheel
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestLineDirectivesInFunctionDecl(t *testing.T) {
+	src := []byte("package main\n\nfunc hello() string {\n\treturn \"world\"\n}\n")
+	goCode, err := TranspileWithOptions(src, TranspileOptions{SourceFile: "test.fw"})
+	if err != nil {
+		t.Fatalf("transpile: %v", err)
+	}
+	if !strings.Contains(goCode, "//line test.fw:") {
+		t.Errorf("missing //line directive:\n%s", goCode)
+	}
+}
+
+func TestNoLineDirectiveWithoutSourceFile(t *testing.T) {
+	src := []byte("package main\n\nfunc hello() string {\n\treturn \"world\"\n}\n")
+	goCode, err := Transpile(src)
+	if err != nil {
+		t.Fatalf("transpile: %v", err)
+	}
+	if strings.Contains(goCode, "//line") {
+		t.Error("should not have //line when no source file")
+	}
+}
